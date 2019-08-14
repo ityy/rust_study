ae5815481146de4054ded405ea326d596c1c77d7
yangyang <ofyang@qq.com>
2019-08-14 15:49:15 +0800

    增加web server

--- src/web_server/iron_router.rs
+++ src/web_server/iron_router.rs
@@ -16,13 +16,13 @@ struct Greeting {
 }
 
 pub fn main() {
-    let port = "localhost:3000";
+    let port = "localhost:80";
 
     //创建路由
     let mut router = Router::new();
     //设置路由
     router.get("/", hello_world, "hello_world");
-    router.post("/set", set_greeting, "set_greeting");
+    router.post("/set/*", set_greeting, "set_greeting");
 
     println!("binding port: {}", port);
     println!("server is running...");
@@ -44,6 +44,20 @@ fn set_greeting(request: &mut Request) -> IronResult<Response> {
     let mut payload = String::new();
     //读取body
     request.body.read_to_string(&mut payload);
+    println!("path:{}", request.url.to_string());
+    let pathV = request.url.path();
+    for s in pathV {
+        println!("sub path:{}", s);
+    }
+
+    //取header
+    let a = request.headers.get_raw("userid");
+    let s: String = match a {
+        Some(s) => String::from_utf8(s[0].clone()).unwrap(),
+        None => "".into(),
+    };
+    println!("userid:{}", s);
+
     //反序列化到对象
     let request: Greeting = serde_json::from_str(payload.as_str()).unwrap();
     let greeting = Greeting {
